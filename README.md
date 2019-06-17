aa702475cb79192ca7c63e0b8040a77f291df3b2
TREE Industries <30479526+TREE-Ind@users.noreply.github.com>
2019-06-17 11:54:02 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# wiktionary
-Wiktionary skill to define words
+## Wiktionary Fallback
+A fallback skill to lookup information about a word.
+
+## Description
+The wiktionary skill is a fallback to find information about a word including definitions, examples, synonymns, etc. using the wiktionary api.
+
+## Examples
+ - "What is the definition for"
+ - "Tell me the definition of"
+ - "Find the definition for"
+
+
+## Credits
+TREE Industries
+
+
---
+++ __init__.py
@@ -0,0 +1,32 @@
+from mycroft import MycroftSkill, intent_file_handler
+from mycroft.audio import wait_while_speaking
+from mycroft.skills.context import *
+from wiktionaryparser import WiktionaryParser
+from mycroft.util.log import LOG
+
+
+class WiktionarySkill(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    def initialize(self):
+        self.register_entity_file("word.entity")
+        self.parser = WiktionaryParser()
+
+    @intent_file_handler('fallback.wiktionary.definition.intent')
+    def handle_wiktionary_definition(self, message):
+        #Get word to define from utterance
+        word = message.data.get('word')
+        #Lookup the word using Wiktionary
+        get_word_info = self.parser.fetch(word)
+        #Get first definition from wiktionary response
+        response = get_word_info[0]['definitions'][0]['text'][1]
+        #Log the definition
+        LOG.info(response)
+        #Speak definition for requested word back to user
+        self.speak_dialog('fallback.wiktionary', {'word': word, 'definition': response})
+
+
+def create_skill():
+    return WiktionarySkill()
+
